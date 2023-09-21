7da0494dc4fd5045869b4bfb5098cf3387645d4c
Alidilawar <59058098+Alidilawar@users.noreply.github.com>
2023-09-21 17:55:54 +0500

    Authentication

---
+++ LoginLogOutMvc/LoginLogOutMvc/Controllers/AccountController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoginLogOutMvc.Models;
+using System.Web.Security;
+
+namespace LoginLogOutMvc.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(MemberShip model)
+        {
+            using (var context = new OfficeEntities())
+            {
+                string key = "drfjkjdvkvdvvfbkbv12154gffgfsdvv";
+                int n = key.Length;
+                var UserNameEncrypt = Utilities.Encrypt(key, model.UserName);
+                var PsdEncrypt = Utilities.Encrypt(key, model.Password);
+
+                var UsrName = Utilities.Decrypt(key, UserNameEncrypt);
+                var Password = Utilities.Decrypt(key, PsdEncrypt);
+
+                bool isvalid = context.Users.Any(x => x.UserName == UsrName && x.Password == Password);
+                if (isvalid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("Index", "Employees");
+                }
+                ModelState.AddModelError("", "Invalid UserName and Password...");
+            }
+            return View();
+        }
+
+        // GET: Account
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signup(User model)
+        {
+            using (var context = new OfficeEntities())
+            {
+                context.Users.Add(model);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ LoginLogOutMvc/LoginLogOutMvc/Models/Utilities.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace LoginLogOutMvc.Models
+{
+    public class Utilities
+    {
+        public static string Encrypt(string key, string plaintxt)
+        {
+            byte[] iv = new byte[16];
+            byte[] array;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream((Stream)ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter sw = new StreamWriter((Stream)cs))
+                        {
+                            sw.Write(plaintxt);
+                        }
+                        array = ms.ToArray();
+                    }
+                }
+            }
+            return Convert.ToBase64String(array);            
+        }
+
+        public static string Decrypt(string key, string ciphertxt)
+        {
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(ciphertxt);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+
+                ICryptoTransform Decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream ms = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cs = new CryptoStream((Stream)ms, Decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader sR = new StreamReader((Stream)cs))
+                        {
+                           return sR.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
